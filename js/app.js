a115ef7304256d80f173e2d7e620a663b5a199ab
Justin Morris <justinjodymorris@gmail.com>
2018-03-26 13:22:41 -0700

    got a working table

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
 'use strict';
+
+Product.allProducts = [];
+
+//find our table id="memory-game"
+var memoryTable = document.getElementById('memory-game');
+
+// Create Table
+
+for (var i = 0; i < 6; i++) {
+  var trElement = document.createElement('tr');
+  for (var j = 0; j < 6; j++) {
+    var tdElement = document.createElement('td');
+    trElement.appendChild(tdElement);
+  }
+  memoryTable.appendChild(trElement);
+}
+
+// Create objects for the images
+function Product(imgName, filePath, link) {
+  this.imgName = imgName;
+  this.filePath = filePath;
+  this.link = link;
+  Product.allProducts.push(this);
+}
+// Create Instances
+// new Product(imgName, filePath, link);
+
+//array of table's td
+
+
