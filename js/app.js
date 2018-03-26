baf60904980b2ae9cfe6ed3c0ac550334d4ddc4f
Justin Morris <justinjodymorris@gmail.com>
2018-03-26 13:40:51 -0700

    renamed images and created successful product instances

--- js/app.js
+++ js/app.js
@@ -2,14 +2,16 @@
 
 Product.allProducts = [];
 
+var tableLevel = 6; // Prints out 6x6 table
+
 //find our table id="memory-game"
 var memoryTable = document.getElementById('memory-game');
 
 // Create Table
 
-for (var i = 0; i < 6; i++) {
+for (var i = 0; i < tableLevel; i++) {
   var trElement = document.createElement('tr');
-  for (var j = 0; j < 6; j++) {
+  for (var j = 0; j < tableLevel; j++) {
     var tdElement = document.createElement('td');
     trElement.appendChild(tdElement);
   }
@@ -25,6 +27,9 @@ function Product(imgName, filePath, link) {
 }
 // Create Instances
 // new Product(imgName, filePath, link);
+new Product ('Bacon Beans', '../img/bacon-beans.jpg', '/products.html#bacon-beans');
+new Product ('Banana Slug Mask', '../img/banana-slug-mask.jpg', '/products.html#banana-slug-mask');
+new Product ('Cat Butt Gum', '../img/cat-butt-gum.jpg', '/products.html#cat-butt-gum');
 
 //array of table's td
 
