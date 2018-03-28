7103878a5f3face4b3d610c14adf0f7249820550
Sam Porter Giffords <travelinsamp@gmail.com>
2018-03-28 16:03:27 -0700

    Local storage gets and uses variables as necessary correctly

---
+++ js/splash.js
@@ -0,0 +1,36 @@
+'use strict'
+
+function localStorageCheck() {
+  var jsonUserName = localStorage.getItem('busmall.playername');
+  var usableStoredUserName = JSON.parse(jsonUserName);
+  if (usableStoredUserName && usableStoredUserName.length) {
+    var initGreeting = document.getElementById('first-greeting');
+    initGreeting.innerHTML = 'Welcome back, ' + usableStoredUserName + '! <br> Wanna play again?';
+
+  }
+}
+localStorageCheck();
+
+var userName;
+var difficulty;
+
+var signInForm = document.getElementById('sign-in-form');
+signInForm.addEventListener('submit', userInput);
+
+function userInput(event) {
+  event.preventDefault();
+  userName = event.target.userName.value;
+  difficulty = event.target.difficulty.value;
+
+
+  var usableUserName = JSON.stringify(userName);
+  var usableDifficulty = JSON.stringify(difficulty);
+  localStorage.setItem('busmall.playername', usableUserName);
+  localStorage.setItem('busmall.difficulty', usableDifficulty);
+
+  window.location = './html/game.html';
+
+}
+
+
+
